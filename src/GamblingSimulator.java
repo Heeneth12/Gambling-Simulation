aca2c61e39b74ed3d963a5e66539a3be01c81313
heeneth <heeneth123@gmail.com>
2024-01-25 12:52:53 +0530

    added logic in uc2

--- src/GamblingSimulator.java
+++ src/GamblingSimulator.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 
 class Stake {
     public int EveryDayStack;
@@ -6,23 +7,27 @@ class Stake {
     public Stake() {
         EveryDayStack = 100;
         EveryGameStack = 1;
-
     }
 
     @Override
     public String toString() {
         return "Stake [EveryDayStack=" + EveryDayStack + ", EveryGameStack=" + EveryGameStack + "]";
     }
-
 }
 
-public class GamblingSimulater {
+public class GamblingSimulator {
 
     public static void main(String[] args) {
-
+        Random rand = new Random();
         Stake obj = new Stake();
-        System.out.println(obj);
+        int WinOrLose = rand.nextInt(2);
+        // Generates either 0 or 1 randomly
+        if (WinOrLose == 1) {
+            System.out.println("He Won  " + WinOrLose + " stake");
+        } else {
+            System.out.println("He lose " + WinOrLose + " stake");
+
+        }
 
     }
-
 }
